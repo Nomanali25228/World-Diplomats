b88aab1d1964c25574a99ff81249ad2717c3f79b
Noman Ali <nomi4698dg@gmail.com>
2026-01-28 23:48:14 +0500

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import Footer from "./component/footer/Footer";
@@ -47,6 +48,11 @@ export const metadata = {
     description:
       "World Diplomats is a global educational UN simulation platform connecting students and young leaders worldwide.",
   },
+  verification: {
+    other: {
+      "facebook-domain-verification": "q1fq9g2q2871lnc3psb935hj0c7v5o",
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -72,6 +78,34 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
+
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '888320257124254');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=888320257124254&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
 
         <Footer />
         <ScrollToTop />
